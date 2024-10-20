f7c3192a325e944f22a27ee6f38ec0f3fe8d9a17
Haruncakir <harun@harun>
2024-10-20 17:51:49 +0300

    Securing sensitive information

--- src/main/java/com/lsm/LsmApplication.java
+++ src/main/java/com/lsm/LsmApplication.java
@@ -3,10 +3,20 @@ package com.lsm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class LsmApplication {
 
 	public static void main(String[] args) {
+		// Load environment variables from .env file
+        Dotenv dotenv = Dotenv.load();
+        
+        // Set system properties from .env file
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+        
 		SpringApplication.run(LsmApplication.class, args);
 	}
 
