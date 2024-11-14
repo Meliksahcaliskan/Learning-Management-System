54e86978d00cd022c25728bb8266821a7c57da0d
OnurhanTalan <o.talan2021@gtu.edu.tr>
2024-11-14 14:34:16 +0300

    file structure for assignments

--- webapp/src/utils/userOptions.jsx
+++ webapp/src/utils/userOptions.jsx
@@ -27,13 +27,14 @@
 //     ]
 // };
 import NewUser from "../components/MainContents/NewUser/NewUser";
-
+import OfficerAssignments from "../components/MainContents/Assignments/OfficerAssignments/OfficerAssignments";
+import StudentAssignments from "../components/MainContents/Assignments/StudentAssignments/StudentsAssignments";
 
 const userOptionsMap = {
     ADMIN: [
         { title: 'Ana Menü', component: null },
         { title: 'Öğrenci Bul', component: null },
-        { title: 'Ödev Takibi', component: null },
+        { title: 'Ödev Takibi', component: OfficerAssignments },
         { title: 'Yoklama', component: null },
         { title: 'Sınav Sonuçları', component: null },
         { title: 'Duyurular', component: null },
@@ -49,7 +50,7 @@ const userOptionsMap = {
     ],
     STUDENT: [
         { title: 'Ana Menü', component: null },
-        { title: 'Ödevler', component: null },
+        { title: 'Ödevler', component: StudentAssignments },
         { title: 'Devamsızlık', component: null },
         { title: 'Geçmiş Sınavlar', component: null },
         { title: 'Duyurular', component: null }
