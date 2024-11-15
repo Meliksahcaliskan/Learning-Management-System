754f2e5549cf316cf7f6b3c91fe362cfc0a4ed66
Haruncakir <harun@harun>
2024-11-15 15:33:22 +0300

    Error handling

--- src/main/java/com/lsm/config/SecurityConfig.java
+++ src/main/java/com/lsm/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.lsm.config;
 
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
